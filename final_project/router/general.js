2281a7d02a117ffc6d49947c0dae1b7f9be72391
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2023-07-01 01:26:33 +0300

    Implemented /author/author mech

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,18 +21,19 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  let filtered_books = books.filter((book)=>book.isbn===isbn);
-  if(filtered_books.length>0){
-      res.send(JSON.stringify(filtered_books));
-  }else{
-      res.send("Book not found check ISBN!");
-  }
+  return res.send(JSON.stringify(books[isbn]));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let filtered_books = Object.values(books).filter((book)=>book.author===author);
+  if(filtered_books.length>0){
+    res.send(JSON.stringify(filtered_books));
+  }else{
+    res.send(`Books by ${author} not found!`)
+  }
 });
 
 // Get all books based on title

7280dfad3fd763fe450fc78766fb6d38ca42e7b5
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2023-07-01 01:41:50 +0300

    Implemented review by ISBN mech

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,8 +20,8 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  const isbn = req.params.isbn;
-  return res.send(JSON.stringify(books[isbn]));
+
+  return res.send(JSON.stringify(books[req.params.isbn]));
  });
   
 // Get book details based on author
@@ -51,7 +51,9 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let book = books[isbn];
+  return res.send(JSON.stringify(book.reviews));
 });
 
 module.exports.general = public_users;

40bdab7444d9f4979ca5498dd628718311576b65
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2023-07-01 12:35:12 +0300

    Changed books request from async to promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,16 +26,23 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',async function (req, res) {
+public_users.get('/',function (req, res) {
   //Write your code here
-  try{
-    const booksData = await books;
-    return res.send(JSON.stringify(booksData,null));
+  const booksPromise = new Promise((resolve,reject)=>{
+    try{
+      const booksData = books;
+      resolve(booksData);
+    }catch{
+      reject(error);
+    }
 
-  }catch(err){
+  });
+  booksPromise.then((booksData)=>{
+    res.send(JSON.stringify(booksData,null));
+  }).catch((err)=>{
     console.error('Error:',err.message);
-    return res.status(500).send('Internal server Error');
-  }
+    res.status(500).send('Internal Server Error');
+  })
   
 });
 
@@ -47,7 +54,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', function (req, res) {
   //Write your code here
   const author = req.params.author;
   let filtered_books = Object.values(books).filter((book)=>book.author===author);
